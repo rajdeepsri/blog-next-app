1c10def6fa9ef0e2a077352d9144d23981d55ab2
rajdeepsri <rajdeep1999.srivastav@gmail.com>
2024-11-25 12:30:09 +0530

    added drizzle config, updated schema with indexing

--- src/server/schema.ts
+++ src/server/schema.ts
@@ -1,16 +1,25 @@
-import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
+import { index, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
-  id: uuid('id').primaryKey(),
-  name: text('name'),
-  email: text('email'),
+  id: uuid('id').primaryKey().defaultRandom(),
+  name: text('name').notNull(),
+  email: text('email').notNull().unique(),
   createdAt: timestamp('created_at').defaultNow(),
 })
 
-export const Post = pgTable('posts', {
-  id: uuid('id').primaryKey(),
-  title: text('title'),
-  content: text('content'),
-  createdAt: timestamp('created_at').defaultNow(),
-  authorId: uuid('author_id').references(() => users.id),
-})
+export const Post = pgTable(
+  'posts',
+  {
+    id: uuid('id').primaryKey(),
+    title: text('title').notNull(),
+    content: text('content'),
+    createdAt: timestamp('created_at').defaultNow(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    authorId: uuid('author_id').references(() => users.id),
+  },
+  table => {
+    return {
+      authorIdIdx: index('author_id_idx').on(table.authorId),
+    }
+  },
+)
