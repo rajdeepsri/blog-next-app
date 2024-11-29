473a208365668fc485e3dac1022c66a6d8980b3d
rajdeepsri <rajdeep1999.srivastav@gmail.com>
2024-11-29 21:57:50 +0530

    dummy commit

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -18,29 +18,30 @@ export type Post = {
 
 const PostCard: FC<{ post: Post; isAdminPage?: boolean }> = ({ post, isAdminPage }) => {
   return (
-    <div className="min-w-[20rem] flex-1 space-y-2 rounded-lg border border-neutral-500 p-4">
-      <h2 className="text-2xl font-semibold">{post.title}</h2>
-      <p>{post.slicedContent}...</p>
-      <div className="flex items-center justify-between gap-2">
-        <div className="flex flex-wrap items-center gap-2">
-          {post?.tags &&
-            post.tags.map((tag, i) => (
-              <span
-                className={`rounded-sm p-1 text-xs font-semibold text-white ${getTagColor(i)}`}
-                key={tag}
-              >
-                {tag}
-              </span>
-            ))}
-        </div>
-        <div className="flex items-center gap-2">
-          <Link href={`/posts/${post.id}`}>
-            <Button variant="outline">Read More</Button>
-          </Link>
-          {isAdminPage && <DeleteEditPostBtn post={post} />}
-        </div>
-      </div>
-    </div>
+    // <div className="min-w-[20rem] flex-1 space-y-2 rounded-lg border border-neutral-500 p-4">
+    //   <h2 className="text-2xl font-semibold">{post.title}</h2>
+    //   <p>{post.slicedContent}...</p>
+    //   <div className="flex items-center justify-between gap-2">
+    //     <div className="flex flex-wrap items-center gap-2">
+    //       {post?.tags &&
+    //         post.tags.map((tag, i) => (
+    //           <span
+    //             className={`rounded-sm p-1 text-xs font-semibold text-white ${getTagColor(i)}`}
+    //             key={tag}
+    //           >
+    //             {tag}
+    //           </span>
+    //         ))}
+    //     </div>
+    //     <div className="flex items-center gap-2">
+    //       <Link href={`/posts/${post.id}`}>
+    //         <Button variant="outline">Read More</Button>
+    //       </Link>
+    //       {isAdminPage && <DeleteEditPostBtn post={post} />}
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="">yo</div>
   )
 }
 
