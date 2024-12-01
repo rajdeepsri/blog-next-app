a4c22588972e473d2b3e2fdef36791f52515ca52
rajdeepsri <rajdeep1999.srivastav.com>
2024-12-01 21:23:28 +0530

    fix: markdown renderer giving style prop error

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { cn } from '@/lib/utils'
 import React, { FC } from 'react'
 import ReactMarkdown from 'react-markdown'
@@ -5,18 +6,27 @@ import remarkGfm from 'remark-gfm'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
+interface CodeProps {
+  node?: any
+  inline?: any
+  className?: any
+  children?: any
+}
+
+type CodeComponentProps = Omit<CodeProps, 'style'>
+
 const MarkdownRenderer: FC<{ content: string; className?: string }> = ({ content, className }) => {
   return (
     <div
-      className={cn('prose sm:prose-xl dark:prose-invert max-w-full sm:max-w-[1280px]', className)}
+      className={cn('prose max-w-full dark:prose-invert sm:prose-xl sm:max-w-[1280px]', className)}
     >
       <ReactMarkdown
         remarkPlugins={[remarkGfm]}
         components={{
-          code({ className, children, ...props }) {
+          code({ className, children, ...props }: CodeComponentProps) {
             const match = /language-(\w+)/.exec(className || '')
             return match ? (
-              <SyntaxHighlighter style={oneDark} language={match[1]} PreTag="div" {...props}>
+              <SyntaxHighlighter language={match[1]} PreTag="div" {...props} style={oneDark}>
                 {String(children).replace(/\n$/, '')}
               </SyntaxHighlighter>
             ) : (
