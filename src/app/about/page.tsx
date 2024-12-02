03a4063c7c8a3d785a428a314256db9c65e4e033
rajdeepsri <rajdeep1999.srivastav.com>
2024-12-02 12:14:20 +0530

    added aboutme a little

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react'
 
 const About: FC = () => {
-  return <div>About</div>
+  return <div className="pt-28 text-center text-3xl">will add soon</div>
 }
 
 export default About
